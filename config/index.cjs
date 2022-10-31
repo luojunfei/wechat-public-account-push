90a8b39e2b49c2d71e84e2d5a24564f3eb794536
Sherlock Kk <35171536+luojunfei@users.noreply.github.com>
2022-10-31 19:12:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -5,7 +5,36 @@
  * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
  * 自行添加属性，以支持更多个性化功能
  */
-const USER_CONFIG = { "APP_ID": "wx4d8146eb2b96587a", "APP_SECRET": "ecf361581f56d7bbaa25ef9099225cdc", "IS_SHOW_COLOR": false, "CALLBACK_TEMPLATE_ID": "Wj1FmpquwPTq9qFRASLnD4Yd5MWLqHni8JhXnrct8qM", "CALLBACK_USERS": [{ "name": "自己", "id": "oJIRs5np4DJfUlDWz7J26Mj5aiwI" }], "USERS": [{ "name": "洁洁", "id": "oJIRs5np4DJfUlDWz7J26Mj5aiwI", "useTemplateId": "CCQOzgN53DK_ZfeSTrgElKVAib28pW0423mugpYHZgc", "province": "江西", "city": "龙南", "horoscopeDate": "10-31", "horoscopeDateType": "今日", "openUrl": "https://luojunfei.github.io/", "festivals": [{ "type": "生日", "name": "生日", "date": "10-31", "year": "2022" }], "customizedDateList": [{ "keyword": "广告", "date": "2022-10-31" }] }], "SWITCH": { "weather": true, "holidaytts": true, "CIBA": true, "oneTalk": false, "earthyLoveWords": false, "momentCopyrighting": false, "poisonChickenSoup": false, "poetry": false, "horoscope": false, "birthdayMessage": true } }
+const USER_CONFIG = { 
+  "APP_ID": "wx4d8146eb2b96587a", 
+  "APP_SECRET": "ecf361581f56d7bbaa25ef9099225cdc", 
+  "IS_SHOW_COLOR": false, 
+  "CALLBACK_TEMPLATE_ID": "Wj1FmpquwPTq9qFRASLnD4Yd5MWLqHni8JhXnrct8qM", 
+  "CALLBACK_USERS": [{ "name": "自己", "id": "oJIRs5np4DJfUlDWz7J26Mj5aiwI" }], 
+  "USERS": [
+    { "name": "洁洁", 
+    "id": "oJIRs5np4DJfUlDWz7J26Mj5aiwI", 
+    "useTemplateId": "CCQOzgN53DK_ZfeSTrgElKVAib28pW0423mugpYHZgc",
+     "province": "江西", 
+     "city": "赣州", 
+     "horoscopeDate": "10-31", 
+     "horoscopeDateType": "今日", 
+     "openUrl": "https://luojunfei.github.io/", 
+     "festivals": [{ "type": "生日", "name": "生日", "date": "10-31", "year": "2022" }], 
+     "customizedDateList": [{ "keyword": "广告", "date": "2022-10-31" }] }], 
+     "SWITCH": { 
+      "weather": true, 
+      "holidaytts": true, 
+      "CIBA": true, 
+      "oneTalk": false, 
+      "earthyLoveWords": false, 
+      "momentCopyrighting": false, 
+      "poisonChickenSoup": false,
+       "poetry": false, 
+       "horoscope": false,
+        "birthdayMessage": true 
+      }
+    }
 
 module.exports = USER_CONFIG
 
